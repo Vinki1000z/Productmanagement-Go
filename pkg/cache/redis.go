f2c6539805bfb011cd145521298fc852af74eb42
Vinki1000x <vikrantchauhancf2021@gmail.com>
2024-12-09 18:55:17 +0530

    Bug Fixed

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -6,6 +6,8 @@ import (
 	"productManagmentBackend/models"
 	"time"
 
+	"strconv"
+
 	"github.com/go-redis/redis/v8"
 )
 
@@ -23,28 +25,43 @@ func InitRedis(redisURL string) error {
 }
 
 func SetProduct(product *models.Product) error {
+	// Convert product ID to a string properly
+	key := "product:" + strconv.Itoa(int(product.ID))
+
+	// Marshal product data into JSON
 	data, err := json.Marshal(product)
 	if err != nil {
 		return err
 	}
 
-	key := "product:" + string(product.ID)
+	// Store product data in Redis with a 24-hour expiration
 	return RedisClient.Set(ctx, key, data, 24*time.Hour).Err()
 }
 
+// GetProduct retrieves a product from Redis by ID
 func GetProduct(id uint) (*models.Product, error) {
-	key := "product:" + string(id)
+	// Properly convert the uint ID to a string
+	key := "product:" + strconv.Itoa(int(id))
+
+	// Fetch product data from Redis
 	data, err := RedisClient.Get(ctx, key).Bytes()
 	if err != nil {
 		return nil, err
 	}
 
+	// Unmarshal JSON into the Product model
 	var product models.Product
 	err = json.Unmarshal(data, &product)
-	return &product, err
-}
+	if err != nil {
+		return nil, err
+	}
 
+	return &product, nil
+}
 func InvalidateProduct(id uint) error {
-	key := "product:" + string(id)
+	// Properly convert the uint ID to a string
+	key := "product:" + strconv.Itoa(int(id))
+
+	// Delete the key from Redis
 	return RedisClient.Del(ctx, key).Err()
 }
